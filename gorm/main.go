a17a0bf0ff5de9d46659f875ff868f41634c66f6
machenggong1996 <mcg1996@163.com>
2020-11-01 19:24:05 +0800

    gorm使用

--- gorm/main.go
+++ gorm/main.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	. "github.com/machenggong1996/golang-learn/gorm/dbinit"
 	. "github.com/machenggong1996/golang-learn/gorm/models"
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
 	"log"
 )
 
 func main() {
 	//Select()
 	//Create()
-	CreateBatch()
+	//CreateBatch()
+	//First()
+	//DryRunSelect()
+	//TxUse()
+	//TxRollBackPart()
+	TopicClassApiUse()
 }
 
 //创建
@@ -22,7 +29,7 @@ func Create() {
 	log.Println(result.Error)
 }
 
-//TODO 批量插入报错
+//批量插入
 func CreateBatch() {
 	//var topicClasses = []TopicClass{{ClassName: "jinzhu1"}, {ClassName: "jinzhu2"}, {ClassName: "jinzhu3"}}
 	//DbHelper.Create(&topicClasses)
@@ -42,6 +49,70 @@ func CreateBatch() {
 
 func Select() {
 	tc := Topic{}
-	DbHelper.Table("topics").Where("topic_id=?", 1).Find(&tc)
+	DbHelper.Debug().Table("topics").Where("topic_id=?", 1).Find(&tc)
 	fmt.Println(tc)
+}
+
+func First() {
+	tc := Topic{}
+	// 获取第一条记录（主键升序）
+	//DbHelper.Table("topics").Take(&tc)
+	// SELECT * FROM users ORDER BY id LIMIT 1;
+	DbHelper.Table("topics").Last(&tc)
+	fmt.Println(tc)
+}
+
+func DryRunSelect() {
+	tc := Topic{}
+	stmt := DbHelper.Session(&gorm.Session{DryRun: true}).First(&tc, 1).Statement
+	fmt.Println(stmt.SQL.String()) //=> SELECT * FROM `users` WHERE `id` = $1 ORDER BY `id`
+	fmt.Println(stmt.Vars)         //=> []interface{}{1}
+}
+
+//使用事务
+func TxUse() {
+	DbHelper.Transaction(func(tx *gorm.DB) error {
+		// 在事务中执行一些 db 操作（从这里开始，您应该使用 'tx' 而不是 'db'）
+		if err := tx.Create(&TopicClass{ClassName: "Giraffe-err"}).Error; err != nil {
+			// 返回任何错误都会回滚事务
+			return err
+		}
+
+		if err := tx.Create(&TopicClass{ClassName: "Lion-err"}).Error; err != nil {
+			return err
+		}
+		// 返回 nil 提交事务
+		return errors.New("roll back")
+	})
+}
+
+//嵌套事务 回滚部分事务 mysql SavePoint
+func TxRollBackPart() {
+
+	topicClass1 := TopicClass{ClassName: "topic1"}
+	topicClass2 := TopicClass{ClassName: "topic2"}
+	topicClass3 := TopicClass{ClassName: "topic3"}
+
+	DbHelper.Transaction(func(tx *gorm.DB) error {
+		tx.Create(&topicClass1)
+
+		tx.Transaction(func(tx2 *gorm.DB) error {
+			tx2.Create(&topicClass2)
+			return errors.New("rollback topic") // 回滚
+		})
+
+		tx.Transaction(func(tx2 *gorm.DB) error {
+			tx2.Create(&topicClass3)
+			return nil
+		})
+
+		return nil
+	})
+}
+
+//Api查询使用 查询部分数据
+func TopicClassApiUse(){
+	var api []ApiTopicClass
+	DbHelper.Model(&TopicClass{}).Limit(10).Find(&api)
+	fmt.Println(api)
 }
--- gorm/models/Model.go
+++ gorm/models/Model.go
@@ -1,17 +1,36 @@
 package models
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"log"
+	"time"
+)
 
 type Topic struct {
 	TopicID    int    `json:"topicId"`
 	TopicTitle string `json:"topicTitle" binding:"required"`
-	TopicDate time.Time
+	TopicDate  time.Time
 	TopicUrl   string `json:"url" binding:"omitempty,topicUrl"`
 }
 
 type TopicClass struct {
-	ClassId int `gorm:"PRIMARY_KEY"`
-	ClassName string
+	ClassId     int `gorm:"PRIMARY_KEY"`
+	ClassName   string
 	ClassRemark string
-	ClassType string `gorm:"Column:classtype"`
+	ClassType   string `gorm:"Column:classtype"`
+}
+
+type ApiTopicClass struct {
+	ClassId   int
+	ClassName string
+}
+
+func (u *TopicClass) BeforeCreate(tx *gorm.DB) (err error) {
+	log.Println("BeforeCreate")
+	return
+}
+
+func (u *TopicClass) AfterCreate(tx *gorm.DB) (err error) {
+	log.Println("AfterCreate")
+	return
 }
