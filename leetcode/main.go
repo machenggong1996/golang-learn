57d543847e56f8fceac8c9a4dee4be164f8e56e7
machenggong1996 <mcg1996@163.com>
2020-11-28 20:29:04 +0800

    增加算法

--- leetcode/main.go
+++ leetcode/main.go
@@ -7,8 +7,11 @@ import (
 
 func main() {
 
-	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	//arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
 	//array.UniqueOccurrences(arr)
-	res := array.SortByBits(arr)
+	//res := array.SortByBits(arr)
+	arr1 := []int{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}
+	arr2 := []int{2, 1, 4, 3, 9, 6}
+	res := array.RelativeSortArray(arr1, arr2)
 	fmt.Println(res)
 }
